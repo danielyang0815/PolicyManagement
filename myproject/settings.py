061946c42303023d7df05520fbf04096993f8337
danielyang0815 <dan93.yang@gmail.com>
2025-12-20 18:17:08 +0800

    Update settings.py
    Upgrade to Jazzmin UI

--- myproject/settings.py
+++ myproject/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = ['daniel-policy-system.zeabur.app', '127.0.0.1', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
