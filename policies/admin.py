5481269819947a033e6ba38aad125e6a403f1981
danielyang0815 <dan93.yang@gmail.com>
2025-12-21 15:21:07 +0800

    Update admin.py

--- policies/admin.py
+++ policies/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Policy
 from django.template.defaultfilters import register
 
-# 修正 Django 5.x 移除 length_is 的相容性補丁
+# 修正 Django 5.x 移除 length_is 的問題
 @register.filter(name='length_is')
 def length_is(value, arg):
     try:
@@ -12,18 +12,17 @@ def length_is(value, arg):
 
 @admin.register(Policy)
 class PolicyAdmin(admin.ModelAdmin):
-    # 這是對齊 image_53bfcc.png 中定義的正確欄位名稱
+    # 這是對應你的 models.py (image_53bfcc.png) 的正確欄位
     list_display = (
-        'client_name',      # 不是 customer_name
-        'policy_id',        # 不是 policy_number
-        'policy_type',      # 不是 insurance_type
-        'coverage_amount',   # 不是 insurance_amount
-        'start_date', 
+        'client_name', 
+        'policy_id', 
+        'policy_type', 
+        'coverage_amount', 
         'status'
     )
     
-    # 加入搜尋功能 (依據 client_name 或 policy_id)
+    # 確保搜尋欄位也對齊模型名稱
     search_fields = ('client_name', 'policy_id')
     
-    # 加入右側篩選器
+    # 確保篩選器也對齊模型名稱
     list_filter = ('policy_type', 'status')
