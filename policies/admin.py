c6ac24b8bc4b1b456170b49dc211e29f8b7818fc
danielyang0815 <dan93.yang@gmail.com>
2025-12-21 11:28:17 +0800

    Update admin.py

--- policies/admin.py
+++ policies/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Policy
 from django.template.defaultfilters import register
 
-# 修正 Django 5.x 移除 length_is 的問題
+# 修正 Django 5.2 移除 length_is 的相容性問題
 @register.filter(name='length_is')
 def length_is(value, arg):
     try:
@@ -10,11 +10,7 @@ def length_is(value, arg):
     except:
         return False
 
+# 最簡化版設定，只顯示 ID，確保絕對不會報錯
 @admin.register(Policy)
 class PolicyAdmin(admin.ModelAdmin):
-    # 這裡只放 Django 預設一定有的欄位來測試，確保不會噴 E108 錯誤
-    # 如果你之後在 models.py 定義了新的欄位，再回來這裡加
-    list_display = ('id', '__str__') 
-    
-    # 暫時註解掉搜尋與篩選，等網站變綠燈再加
-    # search_fields = ('id',)
+    list_display = ('id', '__str__')
