650a4cacd5d8982635bff61a8d5578fe57421d65
danielyang0815 <dan93.yang@gmail.com>
2025-12-21 10:56:42 +0800

    Update admin.py

--- policies/admin.py
+++ policies/admin.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
 from .models import Policy
+from django.template.defaultfilters import register
+
+# 修正 Django 5.x 移除 length_is 的問題
+@register.filter(name='length_is')
+def length_is(value, arg):
+    try:
+        return len(value) == int(arg)
+    except:
+        return False
 
 @admin.register(Policy)
 class PolicyAdmin(admin.ModelAdmin):
-    # 1. 列表頁面要顯示哪些欄位
-    list_display = ('customer_name', 'policy_number', 'insurance_type', 'insurance_amount', 'start_date')
-    
-    # 2. 加入搜尋框 (可以搜尋姓名或保單編號)
+    # 列表顯示欄位
+    list_display = ('customer_name', 'policy_number', 'insurance_type', 'insurance_amount', 'status')
+    # 加入搜尋框
     search_fields = ('customer_name', 'policy_number')
-    
-    # 3. 加入右側篩選欄 (依保險種類或狀態篩選)
+    # 加入右側篩選器
     list_filter = ('insurance_type', 'status')
