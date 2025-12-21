4ac36cda9cef7e151c26580cff68c6b08f9ae59a
danielyang0815 <dan93.yang@gmail.com>
2025-12-21 11:31:01 +0800

    Update admin.py

--- policies/admin.py
+++ policies/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Policy
 from django.template.defaultfilters import register
 
-# 修正 Django 5.2 移除 length_is 的相容性問題
+# 修正 Django 5.x 移除 length_is 的相容性補丁
 @register.filter(name='length_is')
 def length_is(value, arg):
     try:
@@ -10,7 +10,20 @@ def length_is(value, arg):
     except:
         return False
 
-# 最簡化版設定，只顯示 ID，確保絕對不會報錯
 @admin.register(Policy)
 class PolicyAdmin(admin.ModelAdmin):
-    list_display = ('id', '__str__')
+    # 對齊 models.py 的欄位名稱
+    list_display = (
+        'client_name', 
+        'policy_id', 
+        'policy_type', 
+        'coverage_amount', 
+        'start_date', 
+        'status'
+    )
+    
+    # 加入搜尋功能 (搜尋客戶姓名或保單編號)
+    search_fields = ('client_name', 'policy_id')
+    
+    # 加入右側篩選器 (依保險種類或狀態篩選)
+    list_filter = ('policy_type', 'status')
