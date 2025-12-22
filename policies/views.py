09a21fb229aa87ce483613448bdc2ad2e2ab9195
danielyang0815 <dan93.yang@gmail.com>
2025-12-22 22:23:00 +0800

    Update views.py

--- policies/views.py
+++ policies/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Policy
 
-# Create your views here.
+def policy_search(request):
+    # 取得使用者輸入的關鍵字
+    query = request.GET.get('q', '')
+    
+    # 複雜邏輯處理：如果有人搜尋，就過濾資料；否則顯示全部
+    if query:
+        # 搜尋客戶姓名或保單編號
+        results = Policy.objects.filter(client_name__icontains=query) | \
+                  Policy.objects.filter(policy_id__icontains=query)
+    else:
+        results = Policy.objects.all()
+        
+    return render(request, 'policies/index.html', {
+        'policies': results,
+        'query': query
+    })
