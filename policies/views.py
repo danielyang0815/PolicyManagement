92ecb20de285da14386427a648c73930c4d59343
danielyang0815 <dan93.yang@gmail.com>
2025-12-23 14:13:31 +0800

    Update views.py

--- policies/views.py
+++ policies/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Policy
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def policy_search(request):
     # 取得使用者輸入的關鍵字
     query = request.GET.get('q', '')
